ad120f7c47e60f6e19b02ede7695dd207065c0b0
LuizxDev <luizxfelipe.dev@gmail.com>
2023-07-13 14:29:55 -0300

    atributos da classe cliente finalizado

---
+++ schedulepayment/src/main/java/com/flexpag/schedulepayment/model/Cliente.java
@@ -0,0 +1,30 @@
+package com.flexpag.schedulepayment.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "cpf")
+    private String cpf;
+}

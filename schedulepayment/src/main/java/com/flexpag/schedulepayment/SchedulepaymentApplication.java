f5a5a3eaee5fddb4e3c18173499ff45e2cc19603
LuizxDev <luizxfelipe.dev@gmail.com>
2023-07-13 13:51:11 -0300

    projeto adicionado

---
+++ schedulepayment/src/main/java/com/flexpag/schedulepayment/SchedulepaymentApplication.java
@@ -0,0 +1,13 @@
+package com.flexpag.schedulepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulepaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulepaymentApplication.class, args);
+	}
+
+}
